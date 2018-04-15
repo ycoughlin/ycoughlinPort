a130a53e93c00fc9c1b6dad93555810f6667e73c
ycoughlin <senshine.c@gmail.com>
2018-04-15 16:14:19 -0400

    attempting to set up routes

---
+++ portfolio/src/routes.js
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import { Router, Route, hashHistory } from 'react-router';
+// import Home from './src/pages/home/index.js';
+// import App from './App';
+
+// export default (
+//     <Router history={ hashHistory }>
+//         <Route path="/" component={App}></Route>
+//         <Route path="/" component={Home}></Route>
+//     </Router>
+// );
+
+// not sure if this in needed in new react-router version
